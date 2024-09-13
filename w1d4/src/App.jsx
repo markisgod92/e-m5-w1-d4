b2ea6be89b5c895dc0007bec353618462e41f257
markisgod92 <markisgod92@gmail.com>
2024-09-13 09:49:52 +0200

    setup

---
+++ w1d4/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ w1d4/src/data/data.js
@@ -0,0 +1,5 @@
+export const navlinks = [
+    { id: "nav-1", href: "#", text: "Home"},
+    { id: "nav-2", href: "#", text: "About"},
+    { id: "nav-3", href: "#", text: "Browse"}
+]
