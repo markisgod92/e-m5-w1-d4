a622eb92943558ce1e159d3a85605527b24ec591
markisgod92 <markisgod92@gmail.com>
2024-10-05 21:46:05 +0200

    w4d4 test footer

---
+++ src/components/footer/MyFooter.test.js
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { ThemeContextProvider } from '../../context/Theme'
+import { MyFooter } from './MyFooter'
+
+describe('Test Footer', () => {
+    it('should render correctly', () => {
+        const {getByText} = render(
+            <ThemeContextProvider>
+                <MyFooter />
+            </ThemeContextProvider>
+        )
+
+        const content = getByText('© Epibooks 2024 by MG')
+        expect(content).toBeInTheDocument()
+    })
+})
